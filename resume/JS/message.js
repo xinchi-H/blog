f0d275ddb08fb9fb2c3fb39ddfcf469ded100228
huxc <455908283@qq.com>
2019-09-03 02:58:28 +0800

    无刷新留言板

--- resume/JS/message.js
+++ resume/JS/message.js
@@ -14,7 +14,7 @@ query.find()
         let array = messages.map((item) => item.attributes)
         array.forEach((item) => {
             let li = document.createElement('li')
-            li.innerText = item.content
+            li.innerText = `${item.name}: ${item.content}`
             let messageList = document.querySelector('#messageList')
             messageList.appendChild(li)
         })
@@ -24,13 +24,19 @@ query.find()
 let myForm = document.querySelector('#postMessageForm')
 myForm.addEventListener('submit', function (e) {
     e.preventDefault()
+    let name = myForm.querySelector('input[name=name]').value
     let content = myForm.querySelector('input[name=content]').value
     var Message = AV.Object.extend('Message');
     var message = new Message();
+    message.set('name', name);
     message.set('content', content);
     message.save()
         .then(function (message) {
-            window.location.reload()
+            let li = document.createElement('li')
+            li.innerText = `${message.attributes.name}: ${message.attributes.content}`
+            let messageList = document.querySelector('#messageList')
+            messageList.appendChild(li)
+            myForm.querySelector('input[name=content]').value = ''
             console.log(message)
         })
 })
